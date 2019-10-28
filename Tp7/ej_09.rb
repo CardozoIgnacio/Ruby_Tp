da25825df73f5ac51cf7dfb6d8440ebe22b5090e
Ignacio <cardozojoseignacio@gmail.com>
2019-10-28 16:09:16 -0300

    tp_7

--- Tp7/ej_09.rb
+++ Tp7/ej_09.rb
@@ -6,3 +6,87 @@ volver a la Cola original (reencolarse). Cuando se termine de dar servicio a la 
 (no hay más clientes), debe mostrar la Pila y la Cola generada y comenzar a dar servicio a ambas 
 en forma simultánea (a cada elemento de la pila y a cada elemento de la Cola)hasta que una de las 
 2 estructuras se quede sin clientes. Mostrar la estructura que queda. "
+
+
+class Lista
+    def initialize()
+        @arry=Array.new()
+    end
+
+    def remove(pos)
+        if pos<@arry.size()
+            @arry[pos]=nil
+            @arry=@arry.compact()
+        else
+            puts "Posicion #{pos} no es una posicion valida para eliminar"
+        end
+    end
+    
+
+    def size()
+        return @arry.size()
+    end
+    
+    def print()
+        @arry.each do |a|
+            puts a
+        end
+    end
+
+
+
+
+    protected
+    def add(elem)
+        @arry.push(elem)
+    end
+
+
+    def first()
+        return @arry[0]
+    end
+
+    def last()
+        return @arry.last()
+    end
+end
+
+class Cola < Lista
+
+    def initialize()
+        super()
+    end
+
+    def desencolar()
+        if size()>0
+            aux=first()
+            remove(0)
+            return aux
+        else
+            puts "No hay mas elementos en la Cola"
+        end
+    end
+
+    def encolar(elem)
+        add(elem)
+    end
+end
+
+class Cola_prioridad < Cola
+    def initialize()
+        super()
+        
+    end
+
+end
+
+class Persona
+    def initialize(nombre)
+        @nombre = nombre
+        @num=rand(1.99)
+    end
+
+    def num_cola()
+        return @num
+    end
+end
