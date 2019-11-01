34097bcfeab91817b143fb6640597df620c9ec2c
ignacioC <cardozojoseignacio@gmail.com>
2019-11-01 14:56:37 -0300

    Tp6

---
+++ Tp6/ej_08.rb
@@ -0,0 +1,17 @@
+
+
+
+mascota=Hash.new()
+
+mascota["Firda"]=Array.new()
+mascota["Sarnita"]=Array.new()
+mascota["Pirata"]=Array.new()
+
+racion=[100,"DogChoy","1/4"]
+
+racion.push("5",4,3,2)
+
+mascota["Firda"].push(racion)
+
+print racion
+puts
